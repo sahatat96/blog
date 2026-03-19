69ce75cfdd6711ca093d87adc731e66d2b3d1ca9
sahtetmyatthu <mrngasa96@gmail.com>
2026-03-19 11:32:52 +0630

    Fix subdirectory routing, broken asset paths, and navigation links for /blog deployment

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -10,7 +10,7 @@ export async function GET(context) {
 		site: context.site,
 		items: posts.map((post) => ({
 			...post.data,
-			link: `/blog/${post.id}/`,
+			link: `/blog/${post.id}/`, // This is relative to the site URL, so /blog/id/ works if site is correctly set
 		})),
 	});
 }
